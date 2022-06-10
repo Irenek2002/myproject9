f666173d6dc4934d58da23d4884c3575ff57d38d
Irene <irenek2002910910@gmail.com>
2022-06-10 16:26:33 +0800

    Hello world with class Person

---
+++ src/main/java/com/irene/Hello.java
@@ -0,0 +1,8 @@
+package com.irene;
+
+public class Hello {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.greeting();
+    }
+}
---
+++ src/main/java/com/irene/Person.java
@@ -0,0 +1,9 @@
+package com.irene;
+
+public class Person {
+    public void greeting(){
+        public void greeting(){
+            System.out.println("Hello!");
+        }
+    }
+}
